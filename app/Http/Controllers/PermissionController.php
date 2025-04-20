56293f77d14cff7593769224cd4eeb2c04e91706
luqman jaffary <luqmanjaffary79@gmail.com>
2025-04-20 22:42:24 +0500

    start the project

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PermissionController extends Controller
+{
+    //this method will show the permission page
+    public function index(){
+
+    }
+
+    //this method will show the create permission page
+    public function create(){
+
+    }
+
+    //this method will show the  store permission page
+    public function store(){
+
+    }
+
+    //this method will show the edit permission page
+    public function edit(){
+
+    }
+
+
+    //this method will show the update permission page
+    public function update(){}
+    //this method will show the destroy permission page
+    public function destroy(){}
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container py-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card shadow-sm border-0 rounded-4">
+                    <div class="card-header bg-primary text-white fs-5 fw-semibold rounded-top-4">
+                        {{ __('Dashboard') }}
+                    </div>
+
+                    <div class="card-body bg-light rounded-bottom-4">
+                        @if (session('status'))
+                            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                {{ session('status') }}
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                            </div>
+                        @endif
+
+                        <p class="mb-0 text-secondary fs-6">{{ __('You are logged in!') }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
