a010f57164c6ad6794db46a5e24fc9de1965fde0
luqman jaffary <luqmanjaffary79@gmail.com>
2025-04-26 14:09:44 +0500

    added the list files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,9 +20,15 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+                <div class="d-flex align-items-center gap-4">
+                    <a class="nav-link px-0 {{ request()->routeIs('home') ? 'border-bottom border-primary border-2' : '' }}" href="{{ route('home') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                    <a class="nav-link px-0 {{ request()->is('permission*') ? 'border-bottom border-primary border-2' : '' }}" href="{{ route('permission.index') }}">
+                        Permission
+                    </a>
+                </div>
+
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
--- resources/views/permission/list.blade.php
+++ resources/views/permission/list.blade.php
@@ -13,10 +13,41 @@
                     </div>
 
                     <div class="card-body bg-light rounded-bottom-4">
-                        {{-- Flash Message Component --}}
-                        @if (session('success'))
-                            <x-message type="success" :message="session('success')" />
-                        @endif
+                        <x-message />
+
+                        <div class="table-responsive mt-4">
+                            <table class="table table-bordered table-hover align-middle mb-0">
+                                <thead class="table-primary">
+                                <tr>
+                                    <th style="width: 5%">#</th>
+                                    <th>Name</th>
+                                    <th>Created</th>
+                                    <th style="width: 20%">Action</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @forelse ($permissions as $permission)
+                                    <tr>
+                                        <td>{{ $permission->id }}</td>
+                                        <td>{{ $permission->name }}</td>
+                                        <td>{{ $permission->created_at->format('d M Y') }}</td>
+                                        <td>
+                                            <a href="#" class="btn btn-sm btn-primary">Edit</a>
+                                            <form action="#" method="POST" class="d-inline">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="4" class="text-center text-muted">No permissions found.</td>
+                                    </tr>
+                                @endforelse
+                                </tbody>
+                            </table>
+                        </div>
 
                     </div>
                 </div>
