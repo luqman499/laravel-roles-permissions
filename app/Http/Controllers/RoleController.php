c108a40f43fc75ebe112f47a81ccc951135a67d3
luqman jaffary <luqmanjaffary79@gmail.com>
2025-04-29 23:40:51 +0500

    working on the roles controller

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    //This method will show role page
+    public function index(){
+
+    }
+
+    //This method will create role page
+    public function create(){
+
+    }
+
+    //This method will store role page
+    public function store(Request $request ){
+
+    }
+}
---
+++ resources/views/roles/create.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container py-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card shadow-sm border-0 rounded-4">
+
+                    {{-- Card Header --}}
+                    <div
+                        class="card-header bg-primary text-white fs-5 fw-semibold rounded-top-4 d-flex justify-content-between align-items-center">
+                        <span>{{ __('Permissions / Create') }}</span>
+                        <a href="{{ route('permission.index') }}" class="btn btn-sm btn-light text-primary fw-semibold">
+                            ← Back
+                        </a>
+                    </div>
+
+                    {{-- Card Body --}}
+                    <div class="card-body bg-light rounded-bottom-4">
+                        @if (session('status'))
+                            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                {{ session('status') }}
+                                <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                        aria-label="Close"></button>
+                            </div>
+                        @endif
+
+                        <form method="POST" action="{{ route('permission.store') }}">
+                            @csrf
+                            <div class="mb-3 col-md-6">
+                                <label for="name" class="form-label text-secondary">Name</label>
+                                <input type="text" class="form-control" name="name" placeholder="Enter name"
+                                       value="{{ old('name') }}">
+                                @error('name')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+
+                        <p class="mb-0 text-secondary fs-6"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
